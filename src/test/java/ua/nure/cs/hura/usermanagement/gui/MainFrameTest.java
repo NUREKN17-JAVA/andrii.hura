e02b09dc757fbcce38d6762148ba7e03d7af31be
Andrii <andrii.hura@nure.ua>
2019-11-05 08:45:43 +0200

    Method find for test components created

--- src/test/java/ua/nure/cs/hura/usermanagement/gui/MainFrameTest.java
+++ src/test/java/ua/nure/cs/hura/usermanagement/gui/MainFrameTest.java
@@ -1,10 +1,15 @@
 package ua.nure.cs.hura.usermanagement.gui;
 
+import java.awt.Component;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.TestHelper;
+import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 public class MainFrameTest extends JFCTestCase {
+
+	private MainFrame mainFrame;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -17,11 +22,20 @@ public class MainFrameTest extends JFCTestCase {
 
 	@Override
 	protected void tearDown() throws Exception {
-		mainFrame.setVisible(false)
+		mainFrame.setVisible(false);
 		getHelper();
 		TestHelper.cleanUp(this);
 		super.tearDown();
 		
 	}
-
+	
+	private Component find(Class<?> componentClass, String name) {
+		NamedComponentFinder finder;
+		finder = new NamedComponentFinder(componentClass, name);
+		finder.setWait(0);
+		Component component = finder.find(mainFrame, 0);
+		assertNotNull("Could not find component '" + name + "'", component);
+		return component;
+	}
+	
 }
