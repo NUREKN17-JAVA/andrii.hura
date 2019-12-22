232a2a976469afef9c26c2729fb962a50d89156d
Andrii <andrii.hura@nure.ua>
2019-12-22 23:28:41 +0200

    MockServletTestCase class implemented

---
+++ src/test/java/ua/nure/cs/hura/usermanagement/web/MockServletTestCase.java
@@ -0,0 +1,39 @@
+package ua.nure.cs.hura.usermanagement.web;
+
+import java.util.Calendar;
+import java.util.Properties;
+
+import com.mockobjects.dynamic.Mock;
+import com.mockrunner.servlet.BasicServletTestCaseAdapter;
+
+import ua.nure.cs.hura.usermanagement.domain.User;
+import ua.nure.cs.hura.usermanagement.db.DaoFactory;
+import ua.nure.cs.hura.usermanagement.db.MockDaoFactory;
+
+public abstract class MockServletTestCase extends BasicServletTestCaseAdapter {
+
+    private Mock mockUserDao;
+
+    protected Mock getMockUserDao() {
+        return mockUserDao;
+    }
+
+    protected void setMockUserDao(Mock mockUserDao) {
+        this.mockUserDao = mockUserDao;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Properties properties = new Properties();
+        properties.setProperty("dao.factory", MockDaoFactory.class.getName());
+        DaoFactory.init(properties);
+        setMockUserDao(((MockDaoFactory) DaoFactory.getInstance()).getMockUserDao());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        getMockUserDao().verify();
+        super.tearDown();
+    }
+}
