7adb4351547304339cf13609b56c3c60220a88f5
Andrii <andrii.hura@nure.ua>
2019-12-28 12:36:47 +0200

    Helper methods for user record search

--- src/main/java/ua/nure/cs/hura/usermanagement/agent/SearchAgent.java
+++ src/main/java/ua/nure/cs/hura/usermanagement/agent/SearchAgent.java
@@ -1,4 +1,7 @@
 package ua.nure.cs.hura.usermanagement.agent;
+import java.util.Collection;
+import ua.nure.cs.hura.usermanagement.db.DaoFactory;
+import ua.nure.cs.hura.usermanagement.db.DatabaseException;
 
 import jade.core.Agent;
 
@@ -14,6 +17,22 @@ public class SearchAgent extends Agent {
 	protected void takeDown() {
 		System.out.println(getAID().getName()+" terminated.");
 		super.takeDown();
+	}
+	
+	public void search(String firstName, String lastName) throws SearchException{
+		try {
+			Collection users = DaoFactory.getInstance().getUserDao().find(firstName,lastName);
+			if(users.size()>0) {
+				showUsers(users);
+			}else {
+
+			}
+		} catch(DatabaseException e) {
+			throw new SearchException(e);
+		}
+	}
+	private void showUsers(Collection user) {
+
 	}
 
 }
