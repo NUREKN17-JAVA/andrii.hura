3d72e8012cb18dc19f1e198d0b86031d81446854
Andrii <andrii.hura@nure.ua>
2019-12-28 12:52:12 +0200

    Message processing agent behavior added

--- src/main/java/ua/nure/cs/hura/usermanagement/agent/SearchAgent.java
+++ src/main/java/ua/nure/cs/hura/usermanagement/agent/SearchAgent.java
@@ -5,6 +5,10 @@ import ua.nure.cs.hura.usermanagement.db.DatabaseException;
 import jade.core.AID;
 import jade.core.Agent;
 import ua.nure.cs.hura.usermanagement.domain.User;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 
 public class SearchAgent extends Agent {
@@ -15,11 +19,29 @@ public class SearchAgent extends Agent {
 	protected void setup() {
 		super.setup();
 		System.out.println(getAID().getName()+" started");
+		DFAgentDescription description = new DFAgentDescription();
+		description.setName(getAID());
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setName("JADE-searching");
+		serviceDescription.setType("searching");
+		description.addServices(serviceDescription);
+		try {
+			DFService.register(this,description);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+
+		addBehaviour(new RequestServer());
 	}
 
 	@Override
 	protected void takeDown() {
 		System.out.println(getAID().getName()+" terminated.");
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
 		super.takeDown();
 	}
 	
