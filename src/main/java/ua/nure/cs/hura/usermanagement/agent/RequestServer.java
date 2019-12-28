e890b25a8bd05078cb42ede081086a39c11e560b
Andrii <andrii.hura@nure.ua>
2019-12-28 12:43:59 +0200

    createReply method implemented

--- src/main/java/ua/nure/cs/hura/usermanagement/agent/RequestServer.java
+++ src/main/java/ua/nure/cs/hura/usermanagement/agent/RequestServer.java
@@ -1,9 +1,13 @@
 package ua.nure.cs.hura.usermanagement.agent;
-
+import java.util.ArrayList;
 import java.util.Collection;
-
+import java.util.Iterator;
+import java.util.StringTokenizer;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import ua.nure.cs.hura.usermanagement.db.DaoFactory;
+import ua.nure.cs.hura.usermanagement.db.DatabaseException;
+import ua.nure.cs.hura.usermanagement.domain.User;
 
 public class RequestServer extends CyclicBehaviour {
 
@@ -29,8 +33,30 @@ public class RequestServer extends CyclicBehaviour {
 	}
 
 	private ACLMessage createReply(ACLMessage message) {
-		// TODO Auto-generated method stub
-		return null;
+		ACLMessage reply = message.createReply();
+		return null;	
+		String content = message.getContent();
+		StringTokenizer tokenizer = new StringTokenizer(content, ",");
+		if(tokenizer.countTokens()==2) {
+			String firstName = tokenizer.nextToken();
+			String lastName = tokenizer.nextToken();
+			Collection users = null;
+			try {
+				users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			} catch(DatabaseException e) {
+				e.printStackTrace();
+				users = new ArrayList(0);
+			}
+			StringBuffer buffer = new StringBuffer();
+			for(Iterator it = users.iterator(); it.hasNext();) {
+				User user = (User) it.next();
+				buffer.append(user.getId()).append(",");
+				buffer.append(user.getFirstName()).append(",");
+				buffer.append(user.getLastName()).append(",");
+			}
+			reply.setContent(buffer.toString());
+		}
+		return reply;
 	}
 
 }
